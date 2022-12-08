1bdd26dba579a8525026ba9437049212b704bf85
Gustavo Bicalho <gustavo@liveflow.io>
2022-12-08 01:20:18 -0800

    much simpler implementation using throw/catch

--- lib/applicative.ex
+++ lib/applicative.ex
@@ -3,174 +3,73 @@ defmodule Applicative do
   Documentation for `Applicative`.
   """
 
-  defmodule DesugaringState do
-    @enforce_keys [:next_temp_id]
-    defstruct [:next_temp_id]
+  defmodule NotOk do
+    defstruct [:value]
   end
 
-  defp desugar_statement(form, %DesugaringState{} = state, opts) do
-    {new_state, steps, result_form} = desugar_expression(form, state, opts)
+  defp desugar_expression({:=, meta, [pattern, expression]}, opts) do
+    desugared_expression = desugar_expression(expression, opts)
 
-    if Enum.any?(steps, &match?({:<-, _, _}, &1)) do
-      {new_state, steps, result_form}
-    else
-      {state, [], form}
-    end
+    {:=, meta, [pattern, desugared_expression]}
   end
 
-  defp desugar_expression({:=, _meta, [pattern, expression]}, %DesugaringState{} = state, opts) do
-    {state, steps, desugared_expression} = desugar_expression(expression, state, opts)
-
-    {state, steps,
-     quote do
-       unquote(pattern) = unquote(desugared_expression)
-     end}
-  end
-
-  defp desugar_expression({marker, _meta, [ok_form]}, %DesugaringState{} = state, %{
-         marker: marker,
-         temp_var_prefix: temp_var_prefix
-       }) do
-    {state, var} = temp(state, temp_var_prefix)
-
-    {state,
-     [
-       quote do
-         {:ok, unquote(var)} <- unquote(ok_form)
-       end
-     ], var}
-  end
-
-  defp desugar_expression({:fn, _, _} = lambda, state, _opts) do
-    {state, [], lambda}
-  end
-
-  defp desugar_expression({:quote, _, _} = lambda, state, _opts) do
-    {state, [], lambda}
-  end
-
-  defp desugar_expression({f, meta, [_ | _] = args}, %DesugaringState{} = state, opts) do
-    {state, steps, desugared_args} =
-      args
-      |> Enum.reduce({state, [], []}, fn arg_form,
-                                         {state, reverse_many_steps, reverse_desugared_args} ->
-        {state, steps, desugared_arg} = desugar_expression(arg_form, state, opts)
-        {state, [steps | reverse_many_steps], [desugared_arg | reverse_desugared_args]}
-      end)
-      |> then(fn {state, reverse_many_steps, reverse_desugared_args} ->
-        {state, Enum.concat(Enum.reverse(reverse_many_steps)),
-         Enum.reverse(reverse_desugared_args)}
-      end)
-
-    {state, steps, {f, meta, desugared_args}}
-  end
-
-  defp desugar_expression(other, state, %{temp_var_prefix: temp_var_prefix}) do
-    {state, var} = temp(state, temp_var_prefix)
-
-    {state,
-     [
-       quote do
-         unquote(var) = unquote(other)
-       end
-     ], var}
-  end
-
-  defp temp(%DesugaringState{} = state, prefix) do
-    i = state.next_temp_id
-    var = make_temp_var(prefix, i)
-    {put_in(state.next_temp_id, i + 1), var}
-  end
-
-  def make_temp_var(prefix, temp_id) do
-    {String.to_atom("#{prefix}#{temp_id}"), [], __MODULE__}
-  end
-
-  defp pop_last(list) do
-    case Enum.reverse(list) do
-      [] -> nil
-      [x | rev_front] -> {Enum.reverse(rev_front), x}
-    end
-  end
-
-  def desugar_ok?(
-        form,
-        %{temp_var_prefix: temp_var_prefix, temp_var_id_init: temp_var_id_init, marker: marker}
-      ) do
-    init_desugaring_state = %DesugaringState{
-      next_temp_id: temp_var_id_init
-    }
-
-    statements =
-      case form do
-        {:__block__, _, statements} -> statements
-        other_form -> [other_form]
+  defp desugar_expression({marker, _meta, [ok_form]}, %{marker: marker}) do
+    quote do
+      case unquote(ok_form) do
+        {:ok, unquote(temp_var())} -> unquote(temp_var())
+        unquote(temp_var()) -> throw(%NotOk{value: unquote(temp_var())})
       end
+    end
+  end
 
-    statements
-    |> Macro.expand(__ENV__)
-    |> Enum.scan({init_desugaring_state, nil, nil}, fn statement, {desugaring_state, _, _} ->
-      desugar_statement(statement, desugaring_state, %{
-        marker: marker,
-        temp_var_prefix: temp_var_prefix
-      })
-    end)
-    # |> Enum.flat_map(fn {_, steps, final_step} -> desugared_forms end)
-    |> pop_last()
-    |> case do
-      nil ->
-        quote do: nil
+  defp desugar_expression({:fn, _, _} = lambda_form, _opts) do
+    lambda_form
+  end
 
-      {[], {_, [], final_result}} ->
-        final_result
+  defp desugar_expression({:quote, _, _} = quote_form, _opts) do
+    quote_form
+  end
 
-      {desugared_statements, {_, final_steps, final_result}} ->
-        steps =
-          Enum.flat_map(desugared_statements, fn
-            {_, steps, result} ->
-              result_step =
-                case result do
-                  {op, _, _} when op in [:<-, :=] -> result
-                  _other -> quote(do: _ = unquote(result))
-                end
+  defp desugar_expression({f, meta, [_ | _] = args}, opts) do
+    {f, meta, Enum.map(args, &desugar_expression(&1, opts))}
+  end
 
-              steps ++ [result_step]
-          end) ++
-            final_steps
+  defp desugar_expression(other, _opts) do
+    other
+  end
 
-        {:with, [], steps ++ [[{:do, final_result}]]}
+  defp temp_var() do
+    quote(do: temp)
+  end
+
+  def desugar_ok?(form, opts) do
+    quote do
+      try do
+        unquote(desugar_expression(form, opts))
+      catch
+        %NotOk{value: unquote(temp_var())} -> unquote(temp_var())
+      end
     end
   end
 
   defmacro ok?(opts \\ [], do: do_block) do
+    %{file: file, line: line, module: module} = __CALLER__
+
     desugar_ok?(do_block, %{
-      temp_var_prefix: Keyword.get(opts, :temp_var_prefix, "temp_"),
-      temp_var_id_init:
-        Keyword.get_lazy(opts, :temp_var_id_init, fn -> :erlang.unique_integer([:positive]) end),
+      context: %{file: file, line: line, module: module},
       marker: Keyword.get(opts, :marker, :ok!)
     })
   end
 
-  def test() do
-    a = {:ok, 4}
-    b = {:ok, 7}
-    c = {:ok, 8}
-
-    result =
-      ok? do
-        foo = ok!(a)
-        bar = ok!(b) + ok!(c)
-        foo + bar
-      end
-
-    # desugars to
-    ^result =
-      with {:ok, temp_1} <- a,
-           foo = temp_1,
-           {:ok, temp_2} <- b,
-           {:ok, temp_3} <- c,
-           bar = temp_2 + temp_3 do
-        foo + bar
-      end
+  defmacro ok!(_) do
+    raise %CompileError{
+      description: "ok! called outside ok? block",
+      line: __CALLER__.line,
+      file: __CALLER__.file
+    }
   end
+
+  # def bad_ok do
+  #   ok!({:ok, 7})
+  # end
 end
--- test/applicative_test.exs
+++ test/applicative_test.exs
@@ -2,97 +2,17 @@ defmodule ApplicativeTest do
   use ExUnit.Case
   doctest Applicative
   import Applicative
+  alias Applicative.NotOk
 
-  Module.register_attribute(__MODULE__, :desugaring_case, accumulate: true)
-
-  @desugaring_case {
-    "empty",
-    quote do
-    end,
-    nil
-  }
-
-  @desugaring_case {
-    "one statement with no ok! calls",
-    quote do
-      a + b + c
-    end,
-    quote do
-      a + b + c
-    end
-  }
-
-  @desugaring_case {
-    "one statement with variables and one ok! call",
-    quote do
-      a + ok!(b) + c
-    end,
-    quote do
-      with unquote(make_temp_var("temp_", 100)) = a,
-           {:ok, unquote(make_temp_var("temp_", 101))} <- b,
-           unquote(make_temp_var("temp_", 102)) = c do
-        unquote(make_temp_var("temp_", 100)) +
-          unquote(make_temp_var("temp_", 101)) +
-          unquote(make_temp_var("temp_", 102))
-      end
-    end
-  }
-
-  @desugaring_case {
-    "basic case",
-    quote do
-      foo = ok!(a)
-      bar = ok!(b) + ok!(c)
-      foo + bar
-    end,
-    quote do
-      with {:ok, unquote(make_temp_var("temp_", 100))} <- a,
-           foo = unquote(make_temp_var("temp_", 100)),
-           {:ok, unquote(make_temp_var("temp_", 101))} <- b,
-           {:ok, unquote(make_temp_var("temp_", 102))} <- c,
-           bar =
-             unquote(make_temp_var("temp_", 101)) +
-               unquote(make_temp_var("temp_", 102)) do
-        foo + bar
-      end
-    end
-  }
-
-  for {name, sugared, desugared} <- @desugaring_case do
-    test "desugaring #{name}" do
-      expected = unquote({:quote, [], [[{:do, desugared}]]})
-
-      assert expected ==
-               desugar_ok?(
-                 unquote({:quote, [], [[{:do, sugared}]]}),
-                 %{
-                   temp_var_prefix: "temp_",
-                   temp_var_id_init: 100,
-                   marker: :ok!
-                 }
-               )
-    end
-  end
-
-  test "run basic case" do
+  test "basic case" do
     a = {:ok, 4}
     b = {:ok, 7}
     c = {:ok, 8}
 
-    result =
-      ok? do
-        foo = ok!(a)
-        bar = ok!(b) + ok!(c)
-        foo + bar
-      end
-
-    # desugars to
-    assert ^result =
-             (with {:ok, temp_1} <- a,
-                   foo = temp_1,
-                   {:ok, temp_2} <- b,
-                   {:ok, temp_3} <- c,
-                   bar = temp_2 + temp_3 do
+    assert 19 ==
+             (ok? do
+                foo = ok!(a)
+                bar = ok!(b) + ok!(c)
                 foo + bar
               end)
   end
